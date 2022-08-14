e6126a60c744284e0d50a091b70ff91b79277138
Alena Elhajassaad <alenaelhajassaad@yahoo.com>
2022-08-14 15:35:53 -0400

    enum updated

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -5,12 +5,13 @@ public enum Currency {
     PENNY(1), NICKLE(5), DIME(10), QUARTER(25);
     //PENNY is a constant object of the Currency class
 
-
-
-    int value;
+    private int value;
 
     Currency(int value) {
         this.value = value;
     }
 
+    public int getValue() {
+        return value;
+    }
 }
